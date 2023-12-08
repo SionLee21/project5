cee0b688ac483aaf75b86b7628c124b7d50aaf48
yeeun <pariscoco1425@gmail.com>
2023-12-08 17:47:50 +0900

    update HomeController

--- src/main/java/com/myapp/util/HomeController.java
+++ src/main/java/com/myapp/util/HomeController.java
@@ -1,4 +1,15 @@
 package com.myapp.util;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("homeName", "Yeeun");
+        model.addAttribute("name", "this will not work");
+        return "home";
+    }
 }
