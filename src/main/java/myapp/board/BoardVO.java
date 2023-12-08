a13c85ccf5db484b7ef19eb07a9f898aaaad717a
yeeun <pariscoco1425@gmail.com>
2023-12-08 16:17:51 +0900

    upload project5

---
+++ src/main/java/myapp/board/BoardVO.java
@@ -0,0 +1,157 @@
+package myapp.board;
+
+import java.sql.Date;
+
+
+public class BoardVO {
+
+    private int seq;
+
+    private String productName;
+
+    private int price;
+
+    private String category;
+
+    private String location;
+
+    private String description;
+
+    private int sellerId;
+
+    private String imageUrl;
+
+    private Date registrationDate;
+
+    private String condition;
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(int sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+
+/*
+    private int seq;
+    private String title;
+    private String writer;
+    private String content;
+    private Date regDate;
+
+
+    public int getSeq() {
+        return seq;
+    }
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+    */
+
+}
